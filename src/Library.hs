74213c57c001fac4cd11004d277eb8572a432666
Alexander Kalinin <akalinin@frba.utn.edu.ar>
2021-06-06 18:01:09 -0300

    Terminado

--- src/Library.hs
+++ src/Library.hs
@@ -1,5 +1,175 @@
 module Library where
 import PdePreludat
 
-doble :: Number -> Number
-doble numero = numero + numero
+--Kalinin Alexander
+
+
+--Modelado de ratones
+
+data Raton=UnRaton{
+    nombre:: String
+,   edad :: Number
+,   peso :: Number
+,   enfermedades :: [Enfermedad]
+}deriving (Show,Eq)
+
+type Enfermedad = String
+
+
+--Punto 1
+cerebro=UnRaton{
+    nombre = "Cerebro"
+,   edad = 9   
+,   peso = 0.2
+,   enfermedades = ["brucelosis", "sarampion", "tuberculosis"] 
+}
+
+bicenterrata=UnRaton{
+    nombre = "Bicenterrata"
+,   edad = 256   
+,   peso = 0.2
+,   enfermedades = [] 
+}
+
+huesudo=UnRaton{
+    nombre = "Huesudo"
+,   edad = 4   
+,   peso = 10
+,   enfermedades = ["alta obesidad", "sinusitis" ] 
+}
+
+
+--Punto 2
+
+type Hierba = Raton->Raton
+
+
+hierbaBuena :: Hierba
+hierbaBuena  = modificarEdad
+
+modificarEdad :: Raton->Raton
+modificarEdad aRaton = aRaton {edad= sqrt (edad aRaton)} 
+
+------
+hierbaVerde :: String->Hierba
+hierbaVerde tipoEnf  = curarEnfermedad tipoEnf
+
+curarEnfermedad :: String->Raton->Raton
+curarEnfermedad tipoEnf aRaton = aRaton{enfermedades=  eliminarEnfer tipoEnf (enfermedades aRaton)   }
+
+eliminarEnfer  :: String->[Enfermedad]->[Enfermedad]
+eliminarEnfer tipoEnf  = filter (condicionTipoEnf tipoEnf) 
+
+condicionTipoEnf :: String->Enfermedad->Bool
+condicionTipoEnf tipoEnf =  (reverse tipoEnf /= ) . darVuelta tipoEnf
+
+darVuelta :: String->Enfermedad->String
+darVuelta tipoEnf enferm = take  (length tipoEnf)  (reverse enferm)
+------
+
+alcachofa :: Hierba
+alcachofa aRaton | peso aRaton > 2  =  modificarPesoPorcentaje 10  aRaton
+                 |  otherwise = modificarPesoPorcentaje 5 aRaton
+
+modificarPesoPorcentaje :: Number->Raton->Raton
+modificarPesoPorcentaje porcentaje aRaton = aRaton {peso= peso aRaton - (peso aRaton * porcentaje/100)}
+
+------
+
+hierbaZort :: Hierba
+hierbaZort aRaton = transformacionPinky aRaton
+
+transformacionPinky :: Raton->Raton
+transformacionPinky aRaton = aRaton {edad= 0, enfermedades=[]}
+--------
+
+hierbaDelDiablo :: Hierba
+hierbaDelDiablo  =  eliminarEnferDiablo . modificarPesoNumero 
+
+modificarPesoNumero :: Raton->Raton
+modificarPesoNumero aRaton = aRaton {peso= max 0 (peso aRaton - 0.1 )}
+
+eliminarEnferDiablo :: Raton->Raton
+eliminarEnferDiablo aRaton = aRaton {enfermedades= quitarEnferm (enfermedades aRaton)}
+
+quitarEnferm :: [Enfermedad]->[Enfermedad]
+quitarEnferm listaEnfermedades = filter (condicionDiablo) listaEnfermedades
+
+condicionDiablo :: Enfermedad->Bool
+condicionDiablo enferm =    length enferm > 10
+----------
+
+
+--Punto 3)
+
+type Medicamento = [Hierba]
+
+comerHierba :: Hierba->Raton->Raton
+comerHierba hierba  = hierba  
+
+tomarMedicamento :: Medicamento->Raton->Raton
+tomarMedicamento medicamento aRaton = foldl (flip comerHierba ) aRaton medicamento
+
+--------
+--a)
+
+pondsAntiAge :: Medicamento
+pondsAntiAge = [hierbaBuena, hierbaBuena, hierbaBuena, alcachofa]
+
+---------
+--b)
+
+type Potencia = Number
+
+listaVacia = []
+
+reduceFatFast ::Potencia-> Medicamento
+reduceFatFast potencia = [hierbaVerde "obesidad"] ++ listaAlcachofas potencia listaVacia
+
+listaAlcachofas :: Potencia->[Hierba]->Medicamento
+listaAlcachofas potencia lista  | length lista /= potencia =  listaAlcachofas potencia ( lista ++ [alcachofa]  )
+                                | otherwise = lista
+---------
+
+
+--c)
+
+sufijosInfecciosas = [ "sis", "itis", "emia", "cocos"]
+
+pdepCilina :: Medicamento
+pdepCilina = [] ++  map hierbaVerde sufijosInfecciosas
+
+
+-- Punto 4)
+
+--a)
+listaNum :: [Number]
+listaNum = [1..]
+
+cantidadIdeal :: (Number->Bool)->Number
+cantidadIdeal condicion =  head (filter condicion listaNum)
+
+
+--b)
+
+lograEstabilizar :: Medicamento->[Raton]->Bool
+lograEstabilizar medicamento = all (condicionEstabilizar medicamento ) 
+
+condicionEstabilizar :: Medicamento->Raton->Bool
+condicionEstabilizar medicamento raton = tieneSobrepeso medicamento raton && tenerMenosEnfer 3 medicamento raton
+ 
+tenerMenosEnfer :: Number->Medicamento->Raton->Bool
+tenerMenosEnfer n medicamento  = (<n). length . enfermedades . tomarMedicamento medicamento 
+
+tieneSobrepeso :: Medicamento->Raton->Bool
+tieneSobrepeso medicamento  =  (<1). peso . tomarMedicamento medicamento    
+
+
+--c)
+encontrarPotenciaIdealFatFast :: [Raton] -> Number
+encontrarPotenciaIdealFatFast ratones = cuantaPotenciaNecesaria 1 ratones
+
+cuantaPotenciaNecesaria :: Number -> [Raton] -> Number
+cuantaPotenciaNecesaria potencia ratones 
+    | lograEstabilizar (reduceFatFast potencia) ratones = potencia
+    | otherwise = cuantaPotenciaNecesaria (potencia+1) ratones 
